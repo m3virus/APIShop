4850a4f5529d2e1f5e82d31be59cf807e845fa53
M3Virus <mryazdani98@outlook.com>
2024-08-17 07:54:46 +0330

    dd

---
+++ ShopAPI.Back.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace ShopAPI.Back.Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
